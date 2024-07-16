5b7b75baa7d4b1fb623a459c27fe1e233de6c716
anushap6571 <146144960+anushap6571@users.noreply.github.com>
2024-07-15 22:21:51 -0500

    fixed bug on remaining lives

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import random
 
 
 def check(word, guess):
+    lives_lost_in_round = 0
+    if len(guess) > 1:
+            if guess == word:
+                print("correct")
+                sys.exit()
+            else: 
+                print("incorrect")
+                lives_lost_in_round += 1
+                return lives_lost_in_round
+
     tracker = 0;
-    lives_lost = 0
     for letter in word:
         if letter == guess:
             print("Right ")
@@ -12,22 +21,21 @@ def check(word, guess):
             print("Wrong ")
             tracker +=1
     if tracker == len(word):
-        lives_lost += 1
-    return lives_lost
-    
-word_list = ["smart", "birthday", "fashion", "python"]
-word = random.choice(word_list)
-lives = 5
-lives_lost = 0
+        lives_lost_in_round += 1
+        print(lives_lost_in_round)
+    return lives_lost_in_round
 
-while lives_lost <= lives:
-    guess = input("Guess a letter or word: ").lower()
-    if len(guess) > 1:
-        if guess == word:
-            print("correct")
-            sys.exit()
-        else: 
-            print("incorrect")
-            lives_lost += 1
-    lives_lost = lives_lost + check(word, guess)
-    print("Remaining Lives: ", (5 - lives_lost))
+def main():
+    word_list = ["smart", "birthday", "fashion", "python"]
+    word = random.choice(word_list)
+    lives = 5
+    lives_lost = 0
+
+    while lives_lost < lives:
+        guess = input("Guess a letter or word: ").lower()
+        
+        lives_lost = lives_lost + check(word, guess)
+        print("Remaining Lives: ", (lives - lives_lost))
+
+if __name__ == '__main__':
+    main()
